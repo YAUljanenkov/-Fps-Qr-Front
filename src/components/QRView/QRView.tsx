5a207ff88fc7c93660d8f43dd78f5403e5547c00
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-03-09 16:45:17 +0300

    Make sidebar

--- src/components/QRView/QRView.tsx
+++ src/components/QRView/QRView.tsx
@@ -1,15 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {Groups, Button, Card, Input} from 'vienna-ui';
-import {ResponseData} from "../../App/App";
+import {QR} from "../../App/App";
 import styles from './QRView.module.css';
 import {token} from "../../private";
 import {Edit} from "vienna.icons";
-
-
-interface QRViewProps {
-    responseData: ResponseData | undefined
-    setStep: (arg: number) => void
-}
+import {LoaderFunctionArgs, useLoaderData} from "react-router-dom";
+import classNames from "classnames";
 
 interface OrderResponseData {
     amount: number,
@@ -24,88 +20,71 @@ interface OrderResponseData {
     }
 }
 
-const QRView: React.FC<QRViewProps> = ({responseData, setStep} : QRViewProps) => {
-    const [isInvalid, setIsInvalid] = useState(false);
+export async function loader({ params }: LoaderFunctionArgs): Promise<QR>  {
+    const response = await fetch(`/qr/${params.qrId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': token
+        }
+    })
+    return response.json();
+}
+
+const QRView = () => {
+    const qrData = useLoaderData() as QR;
     const [sum, setSum] = useState<number>(0);
     const [load, setLoad] = useState(false);
     const [edit, setEdit] = useState(false);
-    const [orderId, setOrderId] = useState<string | undefined>(undefined);
-
-    const chargeQr = async () => {
-        setIsInvalid(true);
-        const response = await fetch("/order/create", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                'Authorization': token
-            },
-            body: JSON.stringify({
-                amount: sum,
-                qr: {
-                    id: responseData?.qrId
-                }
-            })
-        });
-
-        const result: OrderResponseData = await response.json();
-        console.log(result);
-        setOrderId(result.id);
-    }
-
-    const stopQr = async (id: string) => {
-        setIsInvalid(true);
-        await fetch(`/order/delete/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                'Authorization': token
-            },
-            body: JSON.stringify({
-                amount: sum,
-                qr: {
-                    id: responseData?.qrId
-                }
-            })
-        });
-    }
 
     const setQr = async () => {
         if(edit) {
-            setIsInvalid(false);
             setEdit(false);
             return;
         }
         setLoad(true);
-        if (orderId !== undefined) {
-            await stopQr(orderId);
-        }
+        await fetch("/order/create", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            },
+            body: JSON.stringify({
+                amount: sum,
+                qr: {
+                    id: qrData.qrId,
+                }
+            })
+        });
 
-        await chargeQr();
         setLoad(false);
         setEdit(true);
     }
 
+
     return (
       <>
-        <Groups design={'vertical'} alignItems={'center'} justifyContent={"center"}>
-            <img
-                className={styles.qrBorder}
-                src={responseData && responseData.qrUrl}
-                alt="QR"
-                style={{margin: "10px auto"}}
-            />
+          <Card className={classNames(styles.card)}>
+            <Groups design={'vertical'} alignItems={'center'} justifyContent={"center"}>
+                <b>{qrData.qrId}</b>
+                <img
+                    className={styles.qrBorder}
+                    src={qrData.qrUrl}
+                    alt="QR"
+                    style={{margin: "10px auto"}}
+                />
 
 
-            <Card.ContentTitle>Введите сумму для активации QR кода:</Card.ContentTitle>
-            <Groups design={'horizontal'} style={{marginLeft: "50px"}}>
-                <Input disabled={isInvalid} placeholder='Сумма списания' value={sum === 0? "" : String(sum)} onChange={(e) => {
-                    const value = (e.target as HTMLTextAreaElement).value.replace(/[^\d.]/g, '');
-                    setSum(Number(value));
-                    setIsInvalid(value === "")
-                }} />
-                <Button loading={load} disabled={sum === 0} onClick={setQr}>{edit? <Edit/> : 'OK'}</Button>
+                <Card.ContentTitle>Введите сумму для активации QR кода:</Card.ContentTitle>
+                <Groups design={'horizontal'} style={{marginLeft: "50px"}}>
+                    <Input disabled={edit} placeholder='Сумма списания' value={sum === 0? "" : String(sum)} onChange={(e) => {
+                        const value = (e.target as HTMLTextAreaElement).value.replace(/[^\d.]/g, '');
+                        setSum(Number(value));
+                    }} />
+                    <Button loading={load} disabled={sum === 0} onClick={setQr}>{edit? <Edit/> : 'OK'}</Button>
+                </Groups>
             </Groups>
-        </Groups>
+          </Card>
       </>
     )
 }
