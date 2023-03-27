d334a908b7f539b1d53ef002e4e5e5aadf6ea85d
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-03-27 23:18:06 +0300

    Rewrite header's path sync

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,32 @@
 import {Header} from "vienna-ui";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import logo from '../../static/logo.jpg';
 import styles from './App.module.css';
-import {Outlet, useNavigate} from "react-router-dom";
+import {Outlet, redirect} from "react-router-dom";
 
 const App = () => {
-    const navigate = useNavigate();
+    const getCurrentLocation = () => {
+        if (window.location.pathname.includes('/orders')) {
+            return '/orders';
+        }
+        return '/qrs';
+    }
+
     const items = [
         {value: '/qrs', label: 'Метки'},
         {value: '/orders', label: 'Платежи'},
     ];
-    const [state, setState] = React.useState(window.location.pathname === '/' ?
-        {value: '/qrs'} : {value: window.location.pathname});
+
+    const [path, setPath] = useState<string>(getCurrentLocation());
     const handleChange = (e: React.FormEvent, value: string) => {
-        setState({value});
+        setPath(value);
     }
 
     useEffect(() => {
-        navigate(state.value)
-    }, [state.value])
+        if (!window.location.pathname.includes(path)) {
+            redirect(path)
+        }
+    }, [path])
 
     return (
         <>
@@ -26,7 +34,7 @@ const App = () => {
                 <img className={styles.logo} alt={'sellable logo'} src={logo}/>
             }
                     items={
-                        <Header.Items onChange={handleChange} value={state.value}>
+                        <Header.Items onChange={handleChange} value={path}>
                             {items.map(({value, label}) => (
                                 <Header.Item key={value} value={value} label={label}/>
                             ))}
